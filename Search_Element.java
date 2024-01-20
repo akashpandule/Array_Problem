412c91560d4b9e2911e430b13cf55af25aadad58
Akash Pandule <98227815+akashpandule@users.noreply.github.com>
2024-01-20 12:52:56 +0530

    Update Search_Element.java

--- Search_Element.java
+++ Search_Element.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 public class Search_Element {
 
     // find the index of an element
-    public static int findIndex(int arr[], int t) {
+    public static int Index(int arr[], int t) {
 
         int index = Arrays.binarySearch(arr, t);
         return (index < 0) ? -1 : index;
     }
 
     public static void main(String[] args) {
-        int[] my_array = { 90, 3, 44, 5, 44, 22, 31 };
+        int[] array = { 90, 3, 44, 5, 44, 22, 31 };
         int t = 3;
 
         // find the index of 5
         System.out.println("Index position of is: "
-                + findIndex(my_array, t));
+                + Index(array, t));
 
     }
 }
